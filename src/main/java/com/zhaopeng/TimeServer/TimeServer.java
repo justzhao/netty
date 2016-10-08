7f49a53132170bc15f5539e7f31a14370524841f
zhaopeng <zp6282154@163.com>
2016-10-08 23:37:19 +0800

    msic:回文数

--- src/main/java/com/zhaopeng/TimeServer/TimeServer.java
+++ src/main/java/com/zhaopeng/TimeServer/TimeServer.java
@@ -4,4 +4,13 @@ package com.zhaopeng.TimeServer;
  * Created by zhaopeng on 2016/10/8.
  */
 public class TimeServer {
+
+    public static void main(String args[]){
+
+        int port=8999;
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+
+    }
+
 }
